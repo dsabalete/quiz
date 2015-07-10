dccf742e5c85adb1eda1ee35b477bc0f552485b3
dsabalete <dsabalete@gmail.com>
2015-07-10 05:54:51 -0400

    Correccion de codigo con la nueva version de sequelize.js

--- models/models.js
+++ models/models.js
@@ -14,16 +14,16 @@ var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; // exporta definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
-sequelize.sync().success(function () {
+sequelize.sync().then(function () {
 	// success(..) ejecuta el manejador una vez creada la tabla
-	Quiz.count().success(function (count) {
+	Quiz.count().then(function (count) {
 		if (count === 0) { // la tabla se inicializa solo si está vacía
 			Quiz.create({ 
 				pregunta: 'Capital de Italia', 
 				respuesta: 'Roma'
-			}).success(function() { 
+			}).then(function() { 
 				console.log('Base de datos inicializada');
 			});
-		};
+		}
 	});
 });
