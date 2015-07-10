6742955ab78397827d7432b6dc21915cd1c21190
David Sabalete <dsabalete@gmail.com>
2015-07-10 11:27:06 +0200

    bug 'respuesta.' arreglado

--- models/models.js
+++ models/models.js
@@ -18,10 +18,12 @@ sequelize.sync().success(function () {
 	// success(..) ejecuta el manejador una vez creada la tabla
 	Quiz.count().success(function (count) {
 		if (count === 0) { // la tabla se inicializa solo si está vacía
-			Quiz.create({ pregunta: 'Capital de Italia', 
-						  respuesta. 'Roma'
-						})
-			.success(function() { console.log('Base de datos inicializada');});
+			Quiz.create({ 
+				pregunta: 'Capital de Italia', 
+				respuesta: 'Roma'
+			}).success(function() { 
+				console.log('Base de datos inicializada');
+			});
 		};
 	});
 });
