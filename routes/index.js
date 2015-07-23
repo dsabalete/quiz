8a984fe7ec2ef8259101583c99d28a96be8f2e80
dsabalete <dsabalete@gmail.com>
2015-07-23 16:57:54 -0400

    Autoload

--- routes/index.js
+++ routes/index.js
@@ -3,11 +3,15 @@ var router = express.Router();
 
 var quizController = require('../controllers/quiz_controllers');
 
-/* GET home page. */
+// Pagina de entrada (home page)
 router.get('/', function(req, res) {
   res.render('index', { title: 'Quiz' });
 });
 
+// Autoload de comandos con :quizId
+router.param('quizId', quizController.load); // autoload :quizId
+
+// Definición de rutas de /quizes
 router.get('/quizes', 						quizController.index);
 router.get('/quizes/:quizId(\\d+)', 		quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', 	quizController.answer);
